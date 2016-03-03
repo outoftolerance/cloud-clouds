10674041156da44e3d2e05b0b6053467a3cace1b
James Howard <james@outoftolerance.com>
2016-03-02 17:26:45 -0800

    added other led strips 2 and 3 and code to fade leds

--- controller.py
+++ controller.py
@@ -38,18 +38,42 @@ print "\nCloud Clouds is starting..."
 #setup GPIO output pins
 print "\nSetting up GPIO pins and libraries..."
 GPIO.setmode(GPIO.BCM)
+#LED Strip 1
 GPIO.setup(0, GPIO.OUT)	#LED Red 1
 GPIO.setup(1, GPIO.OUT)	#LED Green 1
 GPIO.setup(2, GPIO.OUT)	#LED Blue 1
+#LED Strip 2
+GPIO.setup(4, GPIO.OUT)	#LED Red 2
+GPIO.setup(5, GPIO.OUT)	#LED Green 2
+GPIO.setup(6, GPIO.OUT)	#LED Blue 2
+#LED Strip 3
+GPIO.setup(12, GPIO.OUT)	#LED Red 3
+GPIO.setup(13, GPIO.OUT)	#LED Green 3
+GPIO.setup(14, GPIO.OUT)	#LED Blue 3
 
 
 #create an array to store all the led objects
 leds = []
+#LED Strip 1
 led = GPIO.PWM(0, LED_FREQ)
 leds.append(led)
 led = GPIO.PWM(1, LED_FREQ)
 leds.append(led)
 led = GPIO.PWM(2, LED_FREQ)
+leds.append(led)
+#LED strip 2
+led = GPIO.PWM(4, LED_FREQ)
+leds.append(led)
+led = GPIO.PWM(5, LED_FREQ)
+leds.append(led)
+led = GPIO.PWM(6, LED_FREQ)
+leds.append(led)
+#LED strip 3
+led = GPIO.PWM(12, LED_FREQ)
+leds.append(led)
+led = GPIO.PWM(13, LED_FREQ)
+leds.append(led)
+led = GPIO.PWM(14, LED_FREQ)
 leds.append(led)
 
 #create an array to store all the led pin duty cycles
@@ -59,8 +83,27 @@ duty_cycles = [1, 1, 1, 1, 1, 1, 1, 1, 1]
 startLeds(leds, duty_cycles)
 
 #main program loop
+#inifite, just updates the LED duty cycles from the duty_cycles array and drives them up and down
 while (1):
 	print "\nMain program loop started."
-	updateLeds( leds, duty_cycles)
-	time.sleep(0.1)
+	#update the duty cycles from 0% to 100%
+	for i in range(0, 100, 1):
+		for j in range(len(duty_cycles)):
+			duty_cycles[j] = i
 
+		#send new duty cyles to LEDs
+		updateLeds( leds, duty_cycles)
+
+		#wait for some time for the changes to take effect
+		time.sleep(0.2)
+
+	#update the duty cycles from 100% to 0%
+	for i in range(100, 0, -1):
+		for j in range(len(duty_cycles)):
+			duty_cycles[j] = i
+			
+		#send new duty cyles to LEDs
+		updateLeds( leds, duty_cycles)
+
+		#wait for some time for the changes to take effect
+		time.sleep(0.2)
